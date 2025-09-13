6b7d918563cab6f7166162eeca057a33c678e3b9
Nintendocustom <nintendocustomyt@gmail.com>
2025-09-13 12:53:28 +0200

    fixes spelling of "TLSClientExeption"

--- tls_client/exceptions.py
+++ tls_client/exceptions.py
@@ -1,2 +1,11 @@
+class TLSClientException(IOError):
+    """General error with the TLS client"""
+
+
 class TLSClientExeption(IOError):
     """General error with the TLS client"""
+
+    def __init__(self, *args, **kwargs):
+        import warnings
+        warnings.warn("TLSClientExeption is deprecated, use TLSClientException instead")
+        super().__init__(*args, **kwargs)
